09cba850ba972d395d3c29be86f3076f3ad2b16d
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-06 19:38:10 -0800

    Update hotels.lua
    - added hotel name
    - added hotel blip settings

--- configs/hotels.lua
+++ configs/hotels.lua
@@ -1,44 +1,113 @@
 Hotels = {
+    -- Valentine
     {
         hotelId = 1,                                 --Make sure this is a unique number for each hotel (once set do not change it will break!)
         location = vector3(-322.12, 767.12, 121.63), --location of where you will buy and enter the hotel room
+        name = 'Saints Hotel',                       --Name of the hotel
         cost = 40,                                   --cost to buy the hotel room
         invSpace = 100,                              --Amount of inventory room the hotel will have
+        blip = {
+            name = 'Saints Hotel',                   --Name of the blip on the map
+            sprite = -211556852,                     --Default: -211556852 / blip_hotel_bed
+            show = true,                             --Show the blip on the map
+            color = 'WHITE',                         --Color of the blip (Blip Colors Shown in 'main.lua' config)
+        }
     },
+    -----------------------------------------------------
+
+    -- Rhodes
     {
         hotelId = 2,                                  --Make sure this is a unique number for each hotel (once set do not change it will break!)
         location = vector3(1343.59, -1302.07, 77.42), --location of where you will buy and enter the hotel room
+        name = 'Mount View Hotel',                    --Name of the hotel
         cost = 40,                                    --cost to buy the hotel room
         invSpace = 100,                               --Amount of inventory room the hotel will have
+        blip = {
+            name = 'Mount View Hotel',                --Name of the blip on the map
+            sprite = -211556852,                      --Default: -211556852 / blip_hotel_bed
+            show = true,                              --Show the blip on the map
+            color = 'WHITE',                          --Color of the blip (Blip Colors Shown in 'main.lua' config)
+        }
     },
+    -----------------------------------------------------
+
+    -- Saint Denis
     {
         hotelId = 3,                                 --Make sure this is a unique number for each hotel (once set do not change it will break!)
         location = vector3(2671.97, -1219.82, 53.3), --location of where you will buy and enter the hotel room
+        name = 'The Lamarque Hotel',                 --Name of the hotel
         cost = 40,                                   --cost to buy the hotel room
         invSpace = 100,                              --Amount of inventory room the hotel will have
+        blip = {
+            name = 'The Lamarque Hotel',             --Name of the blip on the map
+            sprite = -211556852,                     --Default: -211556852 / blip_hotel_bed
+            show = true,                             --Show the blip on the map
+            color = 'WHITE',                         --Color of the blip (Blip Colors Shown in 'main.lua' config)
+        }
     },
+    -----------------------------------------------------
+
+    -- Saint Denis
     {
         hotelId = 4,                                 --Make sure this is a unique number for each hotel (once set do not change it will break!)
         location = vector3(2769.85, -1337.6, 46.46), --location of where you will buy and enter the hotel room
+        name = 'Hotel Chevalier',                    --Name of the hotel
         cost = 40,                                   --cost to buy the hotel room
         invSpace = 100,                              --Amount of inventory room the hotel will have
+        blip = {
+            name = 'Hotel Chevalier',                --Name of the blip on the map
+            sprite = -211556852,                     --Default: -211556852 / blip_hotel_bed
+            show = true,                             --Show the blip on the map
+            color = 'WHITE',                         --Color of the blip (Blip Colors Shown in 'main.lua' config)
+        }
     },
+    -----------------------------------------------------
+
+    -- Strawberry
     {
         hotelId = 5,                                   --Make sure this is a unique number for each hotel (once set do not change it will break!)
         location = vector3(-1778.39, -375.34, 159.91), --location of where you will buy and enter the hotel room
+        name = 'Trackers Hotel',                       --Name of the hotel
         cost = 40,                                     --cost to buy the hotel room
         invSpace = 100,                                --Amount of inventory room the hotel will have
+        blip = {
+            name = 'Trackers Hotel',                   --Name of the blip on the map
+            sprite = -211556852,                       --Default: -211556852 / blip_hotel_bed
+            show = true,                               --Show the blip on the map
+            color = 'WHITE',                           --Color of the blip (Blip Colors Shown in 'main.lua' config)
+        }
     },
+    -----------------------------------------------------
+
+    -- Blackwater
     {
         hotelId = 6,                                  --Make sure this is a unique number for each hotel (once set do not change it will break!)
         location = vector3(-790.34, -1264.38, 43.63), --location of where you will buy and enter the hotel room
+        name = 'The Blackwater Hotel',                --Name of the hotel
         cost = 40,                                    --cost to buy the hotel room
         invSpace = 100,                               --Amount of inventory room the hotel will have
+        blip = {
+            name = 'The Blackwater Hotel',            --Name of the blip on the map
+            sprite = -211556852,                      --Default: -211556852 / blip_hotel_bed
+            show = true,                              --Show the blip on the map
+            color = 'WHITE',                          --Color of the blip (Blip Colors Shown in 'main.lua' config)
+        }
     },
+    -----------------------------------------------------
+
+    -- Van Horn
     {
         hotelId = 7,                                --Make sure this is a unique number for each hotel (once set do not change it will break!)
         location = vector3(2958.83, 483.01, 47.77), --location of where you will buy and enter the hotel room
+        name = 'Van Horn Inn',                      --Name of the hotel
         cost = 40,                                  --cost to buy the hotel room
         invSpace = 100,                             --Amount of inventory room the hotel will have
+        blip = {
+            name = 'Van Horn Inn',                  --Name of the blip on the map
+            sprite = -211556852,                    --Default: -211556852 / blip_hotel_bed
+            show = true,                            --Show the blip on the map
+            color = 'BRIGHT_BLUE',                  --Color of the blip (Blip Colors Shown in 'main.lua' config)
+        }
     },
+    -----------------------------------------------------
 }
