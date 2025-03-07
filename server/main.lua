7f34ca17c01cc54e2517c3a851a255979017fdc2
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-06 19:40:24 -0800

    Update main.lua
    - changed admin check event to callback

--- server/main.lua
+++ server/main.lua
@@ -1,18 +1,21 @@
-RegisterServerEvent("bcc-housing:AdminCheck", function()
-    local _source, admin = source, false
-    local character = VORPcore.getUser(_source).getUsedCharacter
+-- Check if player is admin to use 'AdminManagementMenuCommand'
+VORPcore.Callback.Register('bcc-housing:CheckIfAdmin', function(source, cb)
+    local src = source
+    local user = VORPcore.getUser(src)
+    if not user then return cb(false) end
+
+    local character = user.getUsedCharacter
     if character.group == Config.adminGroup then
-        TriggerClientEvent("bcc-housing:AdminClientCatch", _source, true)
+        return cb(true)
     end
 
-    if not admin then
-        for k, v in pairs(Config.ALlowedJobs) do
-            if character.job == v.jobname then
-                TriggerClientEvent('bcc-housing:AdminClientCatch', _source, true)
-                break
-            end
+    for _, jobCfg in ipairs(Config.ALlowedJobs) do
+        if character.job == jobCfg.jobname then
+            return cb(true)
         end
     end
+
+    cb(false)
 end)
 
 -- Job check to use NPC Real Estate Agent
