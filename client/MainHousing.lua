ebcb9d09f6fa2ef6d048830a2f18a443745de591
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-06 19:36:41 -0800

    Update MainHousing.lua
    - streamlined hotel loop
    - add colored hotel blips
    - housekeeping

--- client/MainHousing.lua
+++ client/MainHousing.lua
@@ -1,7 +1,8 @@
 -- Insert Your Main Client Side Code Here
-HouseCoords, HouseRadius, HouseId, AdminAllowed, Owner, HouseBlips, OwnedHotels, CreatedFurniture, TpHouse, TpHouseInstance =
-    nil, nil,
-    nil, false, nil, {}, {}, {}, nil, nil
+HouseCoords, HouseRadius, HouseId, Owner, TpHouse, TpHouseInstance = nil, nil, nil, nil, nil, nil
+HouseBlips, CreatedFurniture = {}, {}
+local AdminAllowed = false
+local OwnedHotels = {}
 
 RegisterCommand(Config.AdminManagementMenuCommand, function() -- house creation command
     if AdminAllowed then
@@ -20,31 +21,27 @@ else
     end
 end
 
-RegisterNetEvent('vorp:SelectedCharacter')
-AddEventHandler('vorp:SelectedCharacter', function()
-    local player = GetPlayerServerId(tonumber(PlayerId()))
-    Wait(200)
-    TriggerServerEvent("bcc-housing:getPlayersInfo", player)
-    TriggerServerEvent('bcc-housing:AdminCheck')
+RegisterNetEvent('vorp:SelectedCharacter', function()
+    TriggerServerEvent('bcc-housing:getPlayersInfo')
+    TriggerEvent('bcc-housing:AdminCheck')
     TriggerServerEvent('bcc-housing:HotelDbRegistry')
     TriggerServerEvent('bcc-housing:CheckIfHasHouse')
+    TriggerEvent('bcc-housing:ManageHotelBlips')
 end)
 
 CreateThread(function() -- Devmode area
     if Config.DevMode then
         RegisterCommand(Config.DevModeCommand, function()
-            local player = GetPlayerServerId(tonumber(PlayerId()))
-            Wait(200)
-            TriggerServerEvent("bcc-housing:getPlayersInfo", player)
-            TriggerServerEvent('bcc-housing:AdminCheck')
+            TriggerServerEvent('bcc-housing:getPlayersInfo')
+            TriggerEvent('bcc-housing:AdminCheck')
             TriggerServerEvent('bcc-housing:HotelDbRegistry')
             TriggerServerEvent('bcc-housing:CheckIfHasHouse')
+            TriggerEvent('bcc-housing:ManageHotelBlips')
         end, false)
     end
 end)
 
-RegisterNetEvent('bcc-housing:OwnsHouseClientHandler')
-AddEventHandler('bcc-housing:OwnsHouseClientHandler', function(houseTable, owner)
+RegisterNetEvent('bcc-housing:OwnsHouseClientHandler', function(houseTable, owner)
     local coords = json.decode(houseTable.house_coords)
     HouseCoords = vector3(coords.x, coords.y, coords.z)
     HouseRadius = houseTable.house_radius_limit
@@ -67,87 +64,118 @@ AddEventHandler('bcc-housing:OwnsHouseClientHandler', function(houseTable, owner
     showManageOpt(HouseCoords.x, HouseCoords.y, HouseCoords.z, HouseId) -- Ensure HouseId is passed here
 end)
 
-RegisterNetEvent('bcc-housing:AdminClientCatch', function(var) -- admin check catch
-    if var then
+AddEventHandler('bcc-housing:AdminCheck', function()
+    local isAdmin = VORPcore.Callback.TriggerAwait('bcc-housing:CheckIfAdmin')
+    if isAdmin then
         AdminAllowed = true
     end
 end)
 
--- Hotel area --
 RegisterNetEvent('bcc-housing:MainHotelHandler', function()
     devPrint("Initializing main hotel handler")
-    local PromptGroup = BccUtils.Prompts:SetupPromptGroup()
-    local firstprompt = PromptGroup:RegisterPrompt(_U("promptBuy"), BccUtils.Keys[Config.keys.buy], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
 
-    local PromptGroup2 = BccUtils.Prompts:SetupPromptGroup()
-    local firstprompt2 = PromptGroup2:RegisterPrompt(_U("promptEnterHotel"), BccUtils.Keys[Config.keys.manage], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
+    local buyGroup = BccUtils.Prompts:SetupPromptGroup()
+    local buyPrompt = buyGroup:RegisterPrompt(_U("promptBuy"), BccUtils.Keys[Config.keys.buy], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
 
-    local PromptGroup3 = BccUtils.Prompts:SetupPromptGroup()
-    local firstprompt3 = PromptGroup3:RegisterPrompt(_U("hotelInvName"), BccUtils.Keys[Config.keys.manage], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
+    local enterGroup = BccUtils.Prompts:SetupPromptGroup()
+    local enterPrompt = enterGroup:RegisterPrompt(_U("promptEnterHotel"), BccUtils.Keys[Config.keys.manage], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
 
-    local PromptGroup4 = BccUtils.Prompts:SetupPromptGroup()
-    local firstprompt4 = PromptGroup4:RegisterPrompt(_U("promptLeaveHotel"), BccUtils.Keys[Config.keys.manage], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
+    local inventoryGroup = BccUtils.Prompts:SetupPromptGroup()
+    local inventoryPrompt = inventoryGroup:RegisterPrompt(_U("hotelInvName"), BccUtils.Keys[Config.keys.manage], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
+
+    local leaveGroup = BccUtils.Prompts:SetupPromptGroup()
+    local leavePrompt = leaveGroup:RegisterPrompt(_U("promptLeaveHotel"), BccUtils.Keys[Config.keys.manage], 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
 
     local inHotel, hotelInside, instanceNumber, coordsWhenEntered = false, nil, 0, nil
+
     while true do
-        Wait(5)
-        local plc = GetEntityCoords(PlayerPedId())
+        local sleep = 1000
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+
         if not inHotel then
-            for k, v in pairs(Hotels) do
-                if GetDistanceBetweenCoords(plc.x, plc.y, plc.z, v.location.x, v.location.y, v.location.z, true) < 2 then
+            for _, hotel in pairs(Hotels) do
+                local distance = #(playerCoords - hotel.location)
+                if distance < 2 then
+                    sleep = 0
+                    local isOwned = false
+
                     if #OwnedHotels > 0 then
-                        for r, u in pairs(OwnedHotels) do
-                            if v.hotelId == u then
-                                PromptGroup2:ShowGroup(_U("promptHotel"))
-                                if firstprompt2:HasCompleted() then
-                                    devPrint("Entering hotel: " .. tostring(v.hotelId))
+                        for _, hotelId in pairs(OwnedHotels) do
+                            if hotel.hotelId == hotelId then
+                                isOwned = true
+                                enterGroup:ShowGroup(hotel.name)
+                                if enterPrompt:HasCompleted() then
+                                    local player = PlayerId()
+                                    devPrint("Entering hotel: " .. tostring(hotel.hotelId))
                                     inHotel = true
-                                    hotelInside = v
-                                    coordsWhenEntered = plc
-                                    SetEntityCoords(PlayerPedId(), -325.29, 765.23, 121.64)
-                                    instanceNumber = math.random(1, 100000 + tonumber(GetPlayerServerId(PlayerPedId())))
-                                    VORPcore.instancePlayers(tonumber(GetPlayerServerId(PlayerId())) + instanceNumber)
-                                end
-                            else
-                                PromptGroup:ShowGroup(_U("promptGroupName") .. tostring(v.cost))
-                                if firstprompt:HasCompleted() then
-                                    devPrint("Buying hotel: " .. tostring(v.hotelId))
-                                    TriggerServerEvent('bcc-housing:HotelBought', v)
+                                    hotelInside = hotel
+                                    coordsWhenEntered = playerCoords
+                                    SetEntityCoords(playerPed, -325.29, 765.23, 121.64, false, false, false, false)
+                                    instanceNumber = math.random(1, 100000 + tonumber(GetPlayerServerId(player)))
+                                    VORPcore.instancePlayers(tonumber(GetPlayerServerId(player)) + instanceNumber)
+                                    break
                                 end
                             end
                         end
-                    else
-                        PromptGroup:ShowGroup(_U("promptGroupName") .. tostring(v.cost))
-                        if firstprompt:HasCompleted() then
-                            devPrint("Buying hotel: " .. tostring(v.hotelId))
-                            TriggerServerEvent('bcc-housing:HotelBought', v)
+                    end
+
+                    if not isOwned then
+                        buyGroup:ShowGroup(hotel.name .. _U("promptGroupName") .. tostring(hotel.cost))
+                        if buyPrompt:HasCompleted() then
+                            devPrint("Buying hotel: " .. tostring(hotel.hotelId))
+                            TriggerServerEvent('bcc-housing:HotelBought', hotel)
                         end
                     end
                 end
             end
-        else
-            if GetDistanceBetweenCoords(plc.x, plc.y, plc.z, -325.41, 766.9, 121.63) < 1 then
-                PromptGroup3:ShowGroup(_U("promptHotel"))
-                if firstprompt3:HasCompleted() then
+
+        elseif inHotel and hotelInside then
+            sleep = 0
+            local coords = vector3(-325.41, 766.9, 121.63)
+            local distance = #(playerCoords - coords)
+            if distance < 1 then
+                inventoryGroup:ShowGroup(hotelInside.name)
+                if inventoryPrompt:HasCompleted() then
                     devPrint("Opening hotel inventory: " .. tostring(hotelInside.hotelId))
                     TriggerServerEvent('bcc-housing:HotelInvOpen', hotelInside.hotelId)
                 end
             else
-                PromptGroup4:ShowGroup(_U("promptHotel"))
-                if firstprompt4:HasCompleted() then
-                    devPrint("Leaving hotel: " .. tostring(hotelInside.hotelId))
-                    SetEntityCoords(PlayerPedId(), coordsWhenEntered.x, coordsWhenEntered.y, coordsWhenEntered.z)
-                    inHotel = false
-                    VORPcore.instancePlayers(0) -- removes the player from instance
+                leaveGroup:ShowGroup(hotelInside.name)
+                if leavePrompt:HasCompleted() then
+                    if coordsWhenEntered then
+                        devPrint("Leaving hotel: " .. tostring(hotelInside.hotelId))
+                        SetEntityCoords(PlayerPedId(), coordsWhenEntered.x, coordsWhenEntered.y, coordsWhenEntered.z, false, false, false, false)
+                        inHotel = false
+                        VORPcore.instancePlayers(0) -- removes the player from instance
+                    end
                 end
+            end
+        end
+        Wait(sleep)
+    end
+end)
+
+AddEventHandler('bcc-housing:ManageHotelBlips', function()
+    for _, hotelCfg in pairs(Hotels) do
+        if hotelCfg.blip.show then
+            if not hotelCfg.Blip then
+                hotelCfg.Blip = Citizen.InvokeNative(0x554d9d53f696d002, 1664425300, hotelCfg.location) -- BlipAddForCoords
+                SetBlipSprite(hotelCfg.Blip, hotelCfg.blip.sprite, true)
+                Citizen.InvokeNative(0x9CB1A1623062F402, hotelCfg.Blip, hotelCfg.blip.name) -- SetBlipName
+                Citizen.InvokeNative(0x662D364ABF16DE2F, hotelCfg.Blip, joaat(Config.BlipColors[hotelCfg.blip.color])) -- BlipAddModifier
             end
         end
     end
 end)
 
+RegisterNetEvent('bcc-housing:UpdateHotelTable', function(hotelId) -- event to update the housing table
+    devPrint("Updating housing table with house ID: " .. tostring(hotelId))
+    table.insert(OwnedHotels, hotelId)
+end)
+
 -- Function to clear blips and prompts for a specific house
-RegisterNetEvent('bcc-housing:clearBlips')
-AddEventHandler('bcc-housing:clearBlips', function(houseId)
+RegisterNetEvent('bcc-housing:clearBlips', function(houseId)
     -- Clear the blip for the sold house
     if HouseBlips[houseId] then
         BccUtils.Blips:RemoveBlip(HouseBlips[houseId].rawblip) -- Assuming HouseBlips[houseId] has a rawblip field
@@ -161,9 +189,4 @@ AddEventHandler('bcc-housing:clearBlips', function(houseId)
         end
         HouseBlips = {} -- Clear the table to prevent any stale references
     end
-end)
-
-RegisterNetEvent('bcc-housing:HousingTableUpdate', function(houseId) -- event to update the housing table
-    devPrint("Updating housing table with house ID: " .. tostring(houseId))
-    table.insert(OwnedHotels, houseId)
 end)
